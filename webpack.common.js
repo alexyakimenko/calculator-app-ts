6318a16df9d7912683740321c618aa4d5adf48fe
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-04-11 23:20:27 +0300

    fix: .ts imports

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,9 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
     module: {
         rules: [
             {
