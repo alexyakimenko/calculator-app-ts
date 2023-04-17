298251a573aca5f486974638c24234fa0511b67e
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-04-17 16:56:07 +0300

    optimize calculator logic using Calculator class & fix several bugs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './main.scss'
 import {Theme, ThemeControl} from "./ts/theme"
+import { Calculator } from './ts/calculator'
 
 // Initialize Theme  
 const body: HTMLBodyElement = document.querySelector('body')
@@ -68,90 +69,7 @@ document.addEventListener('keyup', (event) => {
 
 // Calc Controls Logic
 const calcKeys: HTMLDivElement = document.querySelector(".calc__controls")
-let calcValue: string = screen.value
 
-screen.addEventListener('input', () => {
-    calcValue = screen.value
-})
-let lastResult: string = '';
-calcKeys.addEventListener('click', (event) => {
-    if(event.target instanceof Element && event.target.tagName === 'BUTTON') {
-        let el: Element = event.target
-        let dataType: string = el.getAttribute('data-type')
-        let lastChar: string = calcValue[calcValue.length-1] || ''
-        let lastSecondChar: string = calcValue[calcValue.length-2] || '' 
-        if(dataType === 'number') {
-            if(calcValue === lastResult) {
-                calcValue = '0'
-            }
-            if(calcValue === 'Infinity') {
-                calcValue = '0'                
-            }
-            if(calcValue === '0') {
-                calcValue = el.textContent
-            } else {
-                if(lastChar.match(/(\/|\+|\-|\*)/)) {
-                    calcValue += el.textContent
-                } else {
-                    if(lastSecondChar.match(/(\/|\+|\-|\*)/) && lastChar === '0') {
-                        calcValue = calcValue.slice(0,-1) + el.textContent
-                        lastChar = el.textContent
-                    } else {
-                        calcValue += el.textContent
-                    }
-                }
-            }
-        } else if(dataType === 'f_point') {
-            if(calcValue === lastResult) {
-                calcValue = '0'
-                lastChar = '0'
-            }
-            if(lastChar !== '.') {
-                if(!Number.isNaN(+lastChar)) {
-                    calcValue += '.'
-                } else {
-                    calcValue += '0.'
-                }
-            }
-        } else if(dataType === 'operator') {
-            let dataOp: string = el.getAttribute('data-op')
-            if(calcValue.length === 0 || calcValue === '0') {
-                calcValue = `0${dataOp}`
-            } else if(lastChar.match(/(\/|\+|\-|\*)/)) {
-                calcValue = calcValue.slice(0,-1) + dataOp
-            } else {
-                calcValue += dataOp
-            }
+let calc = new Calculator(screen)
 
-        } else if(dataType === 'action') {
-            let dataAction: string = el.getAttribute('data-action')
-            if(dataAction === 'reset') {
-                calcValue = '0'
-            }
-            if(dataAction === 'result') {
-                if(lastChar.match(/(\/|\+|\-|\*)/)) {
-                    calcValue = calcValue.slice(0,-1)
-                }
-                let result: number = eval(calcValue)
-                result = eval(result.toFixed(9).toString())
-                if(Number.isNaN(result)) result = 0
-                lastResult = result.toString()
-                calcValue = result.toString()
-            }
-            if(dataAction === 'delete') {
-                if(calcValue === lastResult) {
-                    calcValue = '0'
-                }
-                if(calcValue === "Infinity") {
-                    calcValue = '0'
-                } else {
-                    calcValue = calcValue.slice(0,-1)
-                    if(calcValue.length === 0) {
-                        calcValue = '0'
-                    }
-                }
-            }
-        }
-        screen.value = calcValue
-    }
-})
+calcKeys.addEventListener('click', (event) => calc.clickHandler(event.target))
---
+++ src/ts/calculator.ts
@@ -0,0 +1,145 @@
+enum KeyType {
+    Number = 'number',
+    F_Point = 'f_point',
+    Operator = 'operator',
+    Action = 'action',
+}
+
+class Calculator {
+    private screen: HTMLInputElement;
+
+    private value: string = '0'
+    private lastResult: string = ''
+
+    constructor(screen: HTMLInputElement) {
+        this.screen = screen
+        screen.value.length ? this.value = screen.value : screen.value = this.value
+    }
+
+    public clickHandler(target: EventTarget) {
+        // Event delegation :3
+        if(!(target instanceof Element) || target.tagName !== 'BUTTON') return 
+        const keyType: KeyType = target.getAttribute('data-type') as KeyType
+
+        switch(keyType) {
+            case KeyType.Number: 
+                this.handleNumber(target)
+                break
+            case KeyType.F_Point:
+                this.handlePoint(target)
+                break
+            case KeyType.Operator:
+                this.handleOperator(target)
+                break
+            case KeyType.Action:
+                this.handleAction(target)
+                break
+            default:
+                return
+        }
+
+       this.screen.value = this.value
+    }
+
+    private handleNumber(key: Element) {
+        const keyContent: string = key.getAttribute('data-content')
+
+        if(this.value === '0' || this.value === this.lastResult) {
+            this.value = keyContent
+            return
+        }
+        // Regex
+        const expression: string[] = this.value.split(/(\/|\+|\-|\*)/)
+        const lastNumber: string = expression[expression.length-1] || ''
+
+        if(lastNumber === '0') {
+           this.value = this.value.slice(0, -1) + keyContent
+           return 
+        }
+
+        this.value += keyContent
+    }
+    private handlePoint(key: Element) {
+        const keyContent: string = key.getAttribute('data-content')
+        // Regex
+        const expression: string[] = this.value.split(/(\/|\+|\-|\*)/)
+        const re: RegExp = new RegExp(keyContent, 'g')
+        const dots: string[] = expression[expression.length-1].match(re) || []
+
+        if(dots.length > 1) return 
+
+
+        if(this.value === this.lastResult) {
+            this.value = '0'
+        }
+
+        if(this.isOperator(this.getLastChar(this.value)) ) {
+           this.value += '0' + keyContent
+           return
+        }
+
+        this.value += keyContent
+    }
+
+    private handleOperator(key: Element) {
+        const operator: string = key.getAttribute('data-op')
+
+        if(this.value === '0') {
+            this.value = '0' + operator
+            return
+        }
+
+        if(this.isOperator(this.getLastChar(this.value))) {
+            this.value = this.value.slice(0, -1) + operator
+            return
+        }
+
+        this.value += operator
+    }
+
+    private handleAction(key: Element) {
+        const action: string = key.getAttribute('data-action')
+
+        switch(action) {
+            case 'reset':
+                this.value = '0'
+                break
+            case 'delete': 
+                if(this.value === this.lastResult) {
+                    this.value = '0'
+                    return
+                }
+                this.value = this.value.slice(0, -1)
+                if(this.value.length === 0) this.value = '0'
+                break
+            case 'result':
+                let result: string = this.getResult().toString()
+                this.lastResult = result 
+                this.value = result
+                break
+        }
+    }
+
+    private getResult(): number {
+        if(this.isOperator(this.getLastChar(this.value))) {
+            this.value = this.value.slice(0,-1)
+        }
+
+        let result: number = eval(eval(this.value).toFixed(9).toString())
+
+        if(Number.isNaN(result)) result = 0
+
+        return result
+    }
+
+    private isOperator(char: string): boolean {
+        return !!char.match(/(\/|\+|\-|\*)/)
+    }
+
+    private getLastChar(str: string, deph: number = 1) {
+        return str[str.length-deph] || ''
+    }
+
+}
+
+export { Calculator }
